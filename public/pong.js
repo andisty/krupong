1f2ede04e10fbca74579564638022274cd6e34d2
Ward Verhoef <bypandad@gmail.com>
2016-07-13 15:28:23 +0200

    commend the first functions

--- public/pong.js
+++ public/pong.js
@@ -1,8 +1,10 @@
+// Function to assure he call the function 60 times per second result: 60fps
 var animate = window.requestAnimationFrame ||
   window.webkitRequestAnimationFrame ||
   window.mozRequestAnimationFrame ||
   function(callback) { window.setTimeout(callback, 1000/60) };
 
+// creating canvas
   var canvas = document.createElement('canvas');
   var width = 400;
   var height = 600;
@@ -10,11 +12,13 @@ var animate = window.requestAnimationFrame ||
   canvas.height = height;
   var context = canvas.getContext('2d');
 
+//function to attach canvas to screen when page loads
   window.onload = function() {
     document.body.appendChild(canvas);
     animate(step);
   };
-
+// IMPORTANT FUNC -Inception- will update all player objects & will render all objects
+//                  and finaly call animate with step function as param to call it again
   var step = function() {
     update();
     render();
@@ -22,8 +26,8 @@ var animate = window.requestAnimationFrame ||
   };
 
   var update = function() {
-
   };
+// defining color and size of pong playfield
   var render = function() {
     context.fillStyle = "#050404";
     context.fillRect(0,0, width, height);
