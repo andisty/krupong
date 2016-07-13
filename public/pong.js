0ddcbed40b68c6fd8b51b4e3a69cd336b4cbf0d3
Fabiano Gonzalez <fabianolgonzales@gmail.com>
2016-07-13 16:22:20 +0200

    axis-aligned min bounding box

--- public/pong.js
+++ public/pong.js
@@ -81,3 +81,39 @@ window.onload = function() {
    computer.render();
    ball.render();
  };
+
+ var update = function() {
+   ball.update(playe.paddle, computer.paddle);
+ };
+
+ Ball.prototype.update = function(paddle1, paddle2) {
+   this.x += this.x_speed;
+   this.y += this.y_speed;
+   var top_x = this.x - 5;
+   var top_y = this.y - 5;
+   var bottom_x = this.x + 5;
+   var bottom_y = this.y + 5;
+
+   if(this.x - 5 < 0) {  //hitting th left wall
+     this.x = 5;
+     this.x_speed = -this.x_speed;
+   } else if(this.x + 5 > 400) {//hitting the right wall
+     this.x = 395;
+     this.x_speed = -this.x_speed;
+   }
+
+   if(this.y < 0 || this.y > 600) { // a point was scored
+     this.x_speed = 0;
+     this.y_speed = 3;
+     this.x = 200;
+     this.y = 300;
+   }
+
+   if(top_y > 300) {
+     if(top_y < (paddle1.y + paddle1.height) && bottom_y > paddle2.y && top_x < (paddle2.x + paddle2.width) && bottom_x > paddle2.x) { //hit the computer's paddle
+       this.y_speed = 3;
+       this.x_speed += (paddle2.x_speed / 2);
+       this.y += this.y_speed;
+     }
+   }
+};
