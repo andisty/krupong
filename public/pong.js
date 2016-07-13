7d465b55ac628654355f1ae7136117e8c83578cc
Ward Verhoef <bypandad@gmail.com>
2016-07-13 17:21:43 +0200

    add interaction between ball and paddle so events will get triggered when the ball hits the paddle

--- public/pong.js
+++ public/pong.js
@@ -27,7 +27,7 @@ var animate = window.requestAnimationFrame ||
   };
 
   var update = function() {
-    ball.update();
+    ball.update(player.paddle, computer.paddle);
   };
 
 // defining color and size of pong playfield
@@ -88,11 +88,6 @@ var animate = window.requestAnimationFrame ||
     context.fillStyle = "#29f709";
     context.fill();
   };
-// adding update method to ball so it can move(animate) towards player
-  Ball.prototype.update = function() {
-    this.x += this.x_speed;
-    this.y += this.y_speed;
-  };
 
 // constructing objects we need for our game
   var player = new Player();
@@ -102,3 +97,44 @@ var animate = window.requestAnimationFrame ||
 // ###################################
 // ########### Animating #############
 // ###################################
+
+// adding update method to ball so it can move(animate) towards player
+  Ball.prototype.update = function(paddle1, paddle2) {
+    this.x += this.x_speed;
+    this.y += this.y_speed;
+    var top_x = this.x - 5;
+    var top_y = this.y - 5;
+    var bottom_x = this.x + 5;
+    var bottom_y = this.y + 5;
+
+    if(this.x - 5 < 0) { //ball hitting left wall
+      this.x = 5;
+      this.x_speed = -this.x_speed;
+    } else if(this.x + 5 > 400) { //ball hitting right wall
+      this.x = 395;
+      this.x_speed = -this.x_speed;
+    }
+
+    if(this.y < 0 || this.y > 600) { // a point was scored
+      this.x_speed = 0;
+      this.y_speed = 3;
+      this.x = 200;
+      this.y = 300;
+    }
+
+    if(top_y > 300) {
+      if(top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y && top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x){
+      //hit player paddle
+      this.y_speed = -3;
+      this.x_speed += (paddle1.x_speed / 2);
+      this.y += this.y_speed;
+      }
+    } else {
+      if(top_y < (paddle2.y + paddle2.height) && bottom_y > paddle2.y && top_x < (paddle2.x + paddle2.width) && bottom_x > paddle2.x){
+      //hit computer paddle
+      this.y_speed = 3;
+      this.x_speed += (paddle2.x_speed / 2);
+      this.y += this.y_speed; 
+      }
+    }
+  };
