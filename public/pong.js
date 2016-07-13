9dcabec0b395d01b65ccefeb61927e48232e126c
Fabiano Gonzalez <fabianolgonzales@gmail.com>
2016-07-13 17:11:56 +0200

    controls for paddles typos corrected

--- public/pong.js
+++ public/pong.js
@@ -35,13 +35,13 @@ window.onload = function() {
     }
  };
 
- Paddle.prototype.move = funtion(x,y) {
+ Paddle.prototype.move = function(x,y) {
    this.x += x;
    this.y += y;
    this.x_speed = x;
    this.y_speed = y;
    if (this.x < 0) { // all the way to the left
-   this.x =;
+   this.x = 0;
    this.x_speed = 0;
  } else if (this.x + this.width > 400) { //all the way to the right
   this.x = 400 - this.width;
