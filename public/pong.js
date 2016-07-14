10e718a8cdc45aea27ab25591f62f7c1f04a237b
andisty <distefano.andrea7@gmail.com>
2016-07-14 14:12:30 +0200

    orizzontal positioning and moving of all elements

--- public/pong.js
+++ public/pong.js
@@ -6,14 +6,14 @@ var animate = window.requestAnimationFrame ||
 
 // constructing objects we need for our game + canvas
 var canvas = document.createElement('canvas');
-var width = 400;
-var height = 600;
+var width = 600;
+var height = 400;
 canvas.width = width;
 canvas.height = height;
 var context = canvas.getContext('2d');
 var player = new Player();
 var computer = new Computer();
-var ball = new Ball(200,300);
+var ball = new Ball(300, 200);
 
 var keysDown = {};
 
@@ -47,27 +47,27 @@ var render = function() {
 };
 
 // Players object, paddle so we can render it
-function Paddle(x, y, width, height) {
-  this.x = x;
+function Paddle(y, x, height, width) {
   this.y = y;
-  this.width = width;
+  this.x = x;
   this.height = height;
-  this.x_speed = 0;
+  this.width = width;
   this.y_speed = 0;
+  this.x_speed = 0;
 }
 
 Paddle.prototype.render = function() {
   context.fillStyle = "#29f709";
-  context.fillRect(this.x, this.y, this.width, this.height);
+  context.fillRect(this.y, this.x, this.height, this.width);
 };
 
 // defines default position of player paddle on canvas
 function Player() {
-  this.paddle = new Paddle(175, 580, 50, 10);
+  this.paddle = new Paddle(580, 175, 10, 50);
 }
 // defines default position of computer(player) paddle on canvas
 function Computer() {
-  this.paddle = new Paddle(175, 10, 50, 10);
+  this.paddle = new Paddle(10, 175, 10, 50);
 }
 
 // To render player and computer paddle on canvas. Using prototype so player and computer inherit properties from paddle object
@@ -80,17 +80,17 @@ Computer.prototype.render = function() {
 };
 
 // defining ball object props. x and y are representing center of circle.
-function Ball(x, y) {
-  this.x = x;
+function Ball(y, x) {
   this.y = y;
-  this.x_speed = 0;
+  this.x = x;
   this.y_speed = 3;
+  this.x_speed = 0;
   this.radius = 5;
 }
 // Once again prototype to inherit above defined Ball props
 Ball.prototype.render = function() {
   context.beginPath();
-  context.arc(this.x, this.y, this.radius, 2 * Math.PI, false);
+  context.arc(this.y, this.x, this.radius, 2 * Math.PI, false);
   context.fillStyle = "#29f709";
   context.fill();
 };
@@ -101,23 +101,23 @@ Ball.prototype.render = function() {
 
 // adding update method to ball so it can move(animate) towards player
 Ball.prototype.update = function (paddle1, paddle2) {
-  this.x += this.x_speed;
   this.y += this.y_speed;
-  var top_x = this.x - 5;
+  this.x += this.x_speed;
   var top_y = this.y - 5;
-  var bottom_x = this.x + 5;
+  var top_x = this.x - 5;
   var bottom_y = this.y + 5;
+  var bottom_x = this.x + 5;
 
 // addig collison detection so when the ball hits the paddle shit happens!
-  if(this.x - 5 < 0) {
+  if(this.x - 5 < 0) { // hitting the left wall
     this.x = 5;
     this.x_speed = -this.x_speed;
-  } else if(this.x + 5 > 400) {
+  } else if(this.x + 5 > 400) { // hitting the right wall
     this.x = 395;
     this.x_speed = -this.x_speed;
   }
 
-  if(this.y < 0 || this.y > 600) {
+  if(this.y < 0 || this.y > 600) { // a point was scored
     this.x_speed = 0;
     this.y_speed = 3;
     this.x = 200;
@@ -126,28 +126,30 @@ Ball.prototype.update = function (paddle1, paddle2) {
 
   if(top_y > 300) {
     if(top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y && top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x) {
-    this.y_speed = -3;
-    this.x_speed += (paddle1.x_speed / 2);
-    this.y += this.y_speed;
+      // hit the player's paddle
+      this.y_speed = -3;
+      this.x_speed += (paddle1.x_speed / 2);
+      this.y += this.y_speed;
     }
   } else {
     if(top_y < (paddle2.y + paddle2.height) && bottom_y > paddle2.y && top_x < (paddle2.x + paddle2.width) && bottom_x > paddle2.x) {
-    this.y_speed = 3;
-    this.x_speed += (paddle2.x_speed / 2);
-    this.y += this.y_speed;
+      // hit the computer's paddle
+      this.y_speed = 3;
+      this.x_speed += (paddle2.x_speed / 2);
+      this.y += this.y_speed;
     }
   }
 };
 
-  // player update function so when you press key 37 you go left 39 you move right
+  // player update function so when you press key 38 you go up 40 you move down
   // the number -4 and 4 define how fast you paddle will travel when you pres left or right now it moves 4 on the canvas
 Player.prototype.update = function() {
   for(var key in keysDown) {
     var value = Number(key);
-    if(value == 37) {
-      this.paddle.move(-4, 0);
-    } else if (value == 39) {
-      this.paddle.move(4, 0);
+    if(value == 38) {
+      this.paddle.move(0, -6);
+    } else if (value == 40) {
+      this.paddle.move(0, 6);
     } else {
       this.paddle.move(0, 0);
     }
@@ -155,19 +157,19 @@ Player.prototype.update = function() {
 };
 
 // paddle move function so when player pressed above defined keys the paddle moves 4 on the canvas
-Paddle.prototype.move = function(x, y) {
+Paddle.prototype.move = function(y, x) {
   this.x += x;
   this.y += y;
   this.x_speed = x;
   this.y_speed = y;
-  if(this.x < 0) {
+  if(this.x < 0) { // all the way to the left
     this.x = 0;
     this.x_speed = 0;
-  } else if (this.x + this.width > 400) {
-    this.x = 400 - this.width;
+  } else if (this.x + this.height > 400) { // all the way to the right
+    this.x = 400 - this.height;
     this.x_speed = 0;
   }
-}
+};
   // #########################################
   // ########### Computer Player #############
   // #########################################
@@ -180,11 +182,11 @@ Computer.prototype.update =function(ball) {
   } else if (diff > 0 && diff > 4) {
     diff = 5;
   }
-  this.paddle.move(diff, 0);
+  this.paddle.move(0, diff);
   if(this.paddle.x < 0) {
     this.paddle.x = 0;
-  } else if (this.paddle.x + this.paddle.width > 400) {
-    this.paddle.x = 400 - this.paddle.width;
+  } else if (this.paddle.x + this.paddle.height > 400) {
+    this.paddle.x = 400 - this.paddle.height;
   }
 };
 
