2bf76cd31f764c0d63cf7de90b6db5cd308797af
Ward Verhoef <bypandad@gmail.com>
2016-07-14 10:44:27 +0200

    removed comment

--- public/pong.js
+++ public/pong.js
@@ -172,25 +172,21 @@ Paddle.prototype.move = function(x, y) {
   // ########### Computer Player #############
   // #########################################
 
-// Computer.prototype.update =function(ball) {
-//   var x_pos = ball.x;
-//   var diff = -((this.paddle.x + (this.paddle.width / 2)) - x_pos);
-//   if(diff < 0 && diff < -4) {
-//     diff = -5;
-//   } else if (diff > 0 && diff > 4) {
-//     diff = 5;
-//   }
-//   this.paddle.move(diff, 0);
-//   if(this.paddle.x < 0) {
-//     this.paddle.x = 0;
-//   } else if (this.paddle.x + this.paddle.width > 400) {
-//     this.paddle.x = 400 - this.paddle.width;
-//   }
-// };
-
-// #########################################
-// ########### Player controls #############
-// #########################################
+Computer.prototype.update =function(ball) {
+  var x_pos = ball.x;
+  var diff = -((this.paddle.x + (this.paddle.width / 2)) - x_pos);
+  if(diff < 0 && diff < -4) {
+    diff = -5;
+  } else if (diff > 0 && diff > 4) {
+    diff = 5;
+  }
+  this.paddle.move(diff, 0);
+  if(this.paddle.x < 0) {
+    this.paddle.x = 0;
+  } else if (this.paddle.x + this.paddle.width > 400) {
+    this.paddle.x = 400 - this.paddle.width;
+  }
+};
 
 // adding keyDown object to track key input
   window.addEventListener("keydown", function(event) {
